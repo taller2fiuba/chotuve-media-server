0d807ab86a64d18dae20b00abc0452ed4b435644
edjus <ejusto@fi.uba.ar>
2020-05-28 23:59:02 -0300

    Agrego endpoint para borrar base solo en desarrollo

--- src/routers/home.js
+++ src/routers/home.js
@@ -3,5 +3,8 @@ const homeController = require("../controllers/HomeController");
 const router = express.Router();
 
 router.get("/ping", homeController.ping);
+if (process.env.NODE_ENV == "development") {
+  router.get("/base_de_datos", homeController.borrar_base);
+}
 
 module.exports = router;
