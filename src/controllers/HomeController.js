0b6e13e7b67126a12b51d6cb126e2ad087e78075
edjus <ejusto@fi.uba.ar>
2020-05-28 23:59:02 -0300

    Agrego endpoint para borrar base solo en desarrollo

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,5 +1,13 @@
+const mongosse = require("mongoose");
+
 module.exports = {
   ping: (req, res) => {
     res.status(200).send({});
+  },
+  borrar_base: (req, res) => {
+    mongosse.connection.db
+      .dropDatabase()
+      .then(res.status(200).send({}))
+      .catch(res.status(500).send({}));
   },
 };
