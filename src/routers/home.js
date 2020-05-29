bab1105ef90de499cab984be419b279a9039cb69
edjus <ejusto@fi.uba.ar>
2020-05-29 00:06:44 -0300

    Cambio método de enpoint para borrar base

--- src/routers/home.js
+++ src/routers/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/ping", homeController.ping);
 if (process.env.NODE_ENV == "development") {
-  router.get("/base_de_datos", homeController.borrar_base);
+  router.delete("/base_de_datos", homeController.borrar_base);
 }
 
 module.exports = router;
