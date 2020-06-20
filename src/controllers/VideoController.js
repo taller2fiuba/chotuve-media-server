bde9dd87a21bfcc72ce0cf9b4e130b1b553105ea
Matias <matiaseiglesias@yahoo.com>
2020-06-20 01:04:45 -0300

    agrego endpoint para modificar la habilitacion de los videos, ademas agrego un query param para obtener videos que te permite discriminar entre videos habilitados y no habilitados

--- src/controllers/VideoController.js
+++ src/controllers/VideoController.js
@@ -3,6 +3,7 @@ const controller = require("./Controller");
 
 const OFFSET_POR_DEFECTO = 0;
 const CANTIDAD_POR_DEFECTO = 10;
+const HABILITACION_POR_DEFECTO = true
 
 exports.crear = (req, res) => {
   const video = new Video({
@@ -27,7 +28,8 @@ exports.obtener = (req, res) => {
   const cantidad = req.query.cantidad
     ? req.query.cantidad
     : CANTIDAD_POR_DEFECTO;
-  Video.paginate({}, { offset: offset, limit: cantidad }).then((resultado) => {
+  const solo_habilitados = req.query.solo_habilitados ? req.query.solo_habilitados : HABILITACION_POR_DEFECTO;  
+  Video.paginate({"habilitado": solo_habilitados}, { offset: offset, limit: cantidad }).then((resultado) => {
     res.status(200).json(resultado.docs);
   });
 };
@@ -36,6 +38,16 @@ exports.obtener_por_id = (req, res) => {
   const id = req.params.id;
   Video.findById(id, (err, video) => {
     if (err) return res.status(404).json({});
+    res.status(200).json(video);
+  });
+};
+
+exports.cambiar_habilitacion = (req, res) => {
+  const id = req.params.id;
+  Video.findById(id, (err, video) => {
+    if (err) return res.status(404).json({});
+    video.habilitado = req.body.habilitado;
+    video.save();
     res.status(200).json(video);
   });
 };
--- src/routers/video.js
+++ src/routers/video.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.post("/", videoController.crear);
 router.get("/", videoController.obtener);
 router.get("/:id", videoController.obtener_por_id);
+router.put("/:id", videoController.cambiar_habilitacion);
 
 module.exports = router;
